495ea48bb4a33ab495a3f10ac6f0f8e07ecbabea
amrashrv <a.shrv94@gmail.com>
2021-12-03 17:45:36 +0300

    circleci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-front');
+    expect(app.title).toEqual('Todos');
   });
 
   it('should render title', () => {
