319712259f7a2674b9aab82f007caaf9c2d73c96
amrashrv <a.shrv94@gmail.com>
2021-12-06 15:05:43 +0300

    refactiring

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadlesCI'],
+    browsers: ['Chrome', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
